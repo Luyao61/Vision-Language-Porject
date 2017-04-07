12138834a5b5e948c77239c057f867925a7f7a18
luyao61 <luyao912@gmail.com>
2017-04-07 12:46:36 -0400

    initial commit

---
+++ Models.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.nn.functional as F
+
+
+
+class ImgLinear(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        # calls the init function of nn.Module.  Dont get confused by syntax,
+        # just always do it in an nn.Module
+        super(ImgLinear, self).__init__()
+
+        self.linear = nn.Linear(input_dim, output_dim)
+
+    def forward(self, image_features):
+        # Pass the image features through the linear layer,
+        return self.linear(image_features)
+
+
+class WordEmbedding(nn.Module):
+
+    def __init__(self, vocab_size, Embedding_dim):
+        # calls the init function of nn.Module.  Dont get confused by syntax,
+        # just always do it in an nn.Module
+        super(ImgLinear, self).__init__()
+
+        self.embeds = nn.Embedding(vocab_size, Embedding_dim)
+
+    def forward(self, word_id):
+        return self.embeds(word_id)
